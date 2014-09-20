30099c943d5da6476f13d0e77de3b05398388f26
olivierrr <oliviersinbox@gmail.com>
2014-09-19 23:20:11 -0700

    fixes

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var file = new static.Server('./');
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response)
-    }).resume();
-}).listen(5000);
+    }).resume()
+}).listen( process.env.PORT || 5000)
